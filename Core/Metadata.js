d941621dde0189c32b9b699d9962c7b05d6ca7fe
Zibx <zibx.mail@gmail.com>
2017-02-14 01:18:14 +0300

    add good comment about metadata

--- Core/Metadata.js
+++ Core/Metadata.js
@@ -8,6 +8,12 @@
 
 module.exports = (function () {
     'use strict';
+
+    /* TODO: transform it to linker. we can not collect metadata,
+       TODO: it is a recursive operation in quokkaScript
+       TODO: we do not know how to traverse a node until
+       TODO: we know how to do it
+     */
     var get = function(){
 
     };
