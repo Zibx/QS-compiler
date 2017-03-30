c42653055a1dc5ed58cc2c6b59dd2c5c4d2352d0
Zibx <zibx.mail@gmail.com>
2017-03-30 21:32:11 +0300

    building progress

--- build.js
+++ build.js
@@ -8,9 +8,30 @@
 
 module.exports = (function () {
     'use strict';
-    var fs = require('fs');
+    var fs = require('fs'),
+        path = require('path')
+        ;
     var showHelp;
 
+    function readDirRecursive(dir) {
+        var entries = fs.readdirSync(dir);
+        var ret = [];
+        for (var i = 0; i < entries.length; i++) {
+            var entry = entries[i];
+            var fullPath = path.join(dir, entry);
+            var stat = fs.statSync(fullPath);
+            if (stat.isFile()) {
+                /*if (entry == "TypeTable.js")
+                 ret.push("module.exports.TypeTable=require('../" + fullPath.replace(/\\/g, "/") + "')");
+                 else*/
+                ret.push(fullPath.replace(/\\/g, "/"));
+            }
+            if (stat.isDirectory()) {
+                ret = ret.concat(readDirRecursive(fullPath));
+            }
+        }
+        return ret;
+    };
 
     var build = function build(cfg){
 
@@ -18,7 +39,24 @@ module.exports = (function () {
         if(!cfg.config)
             showHelp('No config file specified');
 
-        var config = JSON.parse(fs.readFileSync(cfg.config)+'');
+        try {
+            var config = JSON.parse(fs.readFileSync(cfg.config) + '');
+        }catch(e){
+            showHelp('JSON config is corrupted or not exists ('+cfg.config+')', e)
+        }
+
+        console.log('JSON config parsed ('+cfg.config+')');
+        if(!config.lib){
+            showHelp('lib dir is not specified in config');
+        }
+        var libDir = path.resolve(config.basePath || __dirname,config.lib);
+        try {
+            var files = readDirRecursive(libDir);
+        }catch(e){
+            showHelp('Error reading directory ('+libDir+')', e);
+        }
+        console.log(files.map(function(filePath){return path.basename(filePath);}))
+
         console.log(config);
         console.dir(cfg);
     };
@@ -29,8 +67,10 @@ module.exports = (function () {
             throw new Error(error);
         };
     }else{
-        showHelp = function(error){
+        showHelp = function(error, more){
             console.log('Error: '+(error||'Unknown'));
+            if(more)
+                console.log(more);
             console.log('node build -c ******.json -fd');
             process.exit();
         };
