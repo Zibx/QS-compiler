6ada7f9513dee2154da0cf090aebd7cb3e0c9382
Zibx <zibx.mail@gmail.com>
2017-07-10 20:51:09 +0300

    tabs test

---
+++ test/tabsTest.js
@@ -0,0 +1,39 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * *
+ */
+;// QUOKKA 2017
+// By zibx on 7/10/17.
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * *
+ */
+;// QUOKKA 2017
+// By zibx on 5/5/17.
+
+module.exports = (function () {
+    'use strict';
+    var assert = require('chai').assert;
+    var build = require('../build'),
+        esprima = require('esprima'),
+        escodegen = require('escodegen'),
+        fs = require('fs');
+
+    var compile = function(fileName, cb){
+        var crafted = build({
+            lib: 'test/lib/QComponent4/src',
+            typeTable: 'Core/TypeTable.js',
+            source: fs.readFileSync(fileName) + ''
+        }, cb);
+    };
+
+    describe('tabs', function() {
+        compile('test/qs/tabs.qs', function (result) {
+            var main = result.ast.main;
+
+
+        });
+    });
+})();
